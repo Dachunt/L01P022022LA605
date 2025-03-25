b3811489b4d61d4f7d455b84978cd50f1329258b
Diego Hernandez <diego.hernandez4@catolica.edu.sv>
2025-03-25 16:56:55 -0600

    Agregar archivos de proyecto.

---
+++ L01P022022LA605/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P022022LA605.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
